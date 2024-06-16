588208a787eae21dc0b3a86f6a269e5ab5b8db46
Felipe <feferodrigues771@gmail.com>
2024-06-16 17:02:52 -0300

    chore: add Istanbul coverage for test reporting

--- back_end/jest.config.js
+++ back_end/jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.test.ts'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/',
+    '/src/database/', // ignore database files if needed
+    '/src/config/', // ignore config files if needed
+  ],
+};
